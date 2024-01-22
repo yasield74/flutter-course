5127bdc4b3778776601c06f5f811f8776f10fd33
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-01-21 21:12:44 -0700

    Added mappers y factory constructor.

---
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
+
+class GetYesNoAnswer {
+  Future<Message> getAnswer() async {
+    final _dio = Dio();
+    final response = await _dio.get('https://yesno.wtf/api');
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    return Message(
+        text: yesNoModel.answer,
+        fromWho: FromWho.her,
+        imageUrl: yesNoModel.image);
+  }
+}
--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> message = [
     Message(text: 'Hola', fromWho: FromWho.me),
     Message(text: 'Ya regresaste', fromWho: FromWho.me)
   ];
 
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    message.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
 
     final newMessage = Message(text: text, fromWho: FromWho.me);
     message.add(newMessage);
+
+    if (text.endsWith('?')) {
+      await herReply();
+    }
     notifyListeners();
     moveScrollToBottom();
   }
