91ed8341c400e46f15b51a00e4c6f543b6eb9fce
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-02-03 00:28:04 -0700

    Added Hooks to the classes to simplify code and state management.

--- flutter_foundations_rezo/lib/randomizer_page.dart
+++ flutter_foundations_rezo/lib/randomizer_page.dart
@@ -1,41 +1,32 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class RandomizerPage extends StatefulWidget {
-  const RandomizerPage({super.key, required this.min, required this.max});
+class RandomizerPage extends HookWidget {
+  RandomizerPage({super.key, required this.min, required this.max});
   final int min, max;
 
-  @override
-  State<RandomizerPage> createState() => _RandomizerPageState();
-}
-
-class _RandomizerPageState extends State<RandomizerPage> {
-  int? _generatedNumber;
   final randomGenerator = Random();
 
   @override
   Widget build(BuildContext context) {
+    final _generatedNumber = useState<int?>(null);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Randomizer')),
       body: Center(
         child: Text(
-          _generatedNumber?.toString() ?? 'Generate a number',
+          _generatedNumber.value?.toString() ?? 'Generate a number',
           style: const TextStyle(fontSize: 42),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
-            setState(() {
-              _generatedNumber = widget.min +
-                  randomGenerator.nextInt(widget.max - widget.min + 1);
-              print(_generatedNumber);
-            });
+            _generatedNumber.value =
+                min + randomGenerator.nextInt(max + 1 - min);
           },
           label: const Text('Generate')),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
-
-
-//TODO: 1:40hrs
--- flutter_foundations_rezo/lib/range_selector_page.dart
+++ flutter_foundations_rezo/lib/range_selector_page.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foundations_rezo/randomizer_page.dart';
 import 'package:flutter_foundations_rezo/range_selected_form.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class RangeSelectorPage extends StatefulWidget {
-  const RangeSelectorPage({super.key});
-
-  @override
-  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
-}
-
-class _RangeSelectorPageState extends State<RangeSelectorPage> {
-  int _min = 0;
-  int _max = 0;
+class RangeSelectorPage extends HookWidget {
+  RangeSelectorPage({super.key});
   final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+    final min = useState<int>(0);
+    final max = useState<int>(0);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
       body: RangeSelectorForm(
-        minValueSetter: (value) => _min = value,
-        maxValueSetter: (value) => _max = value,
+        minValueSetter: (value) => min.value = value,
+        maxValueSetter: (value) => max.value = value,
         formKey: formKey,
       ),
       floatingActionButton: FloatingActionButton(
@@ -31,7 +27,7 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
           if (formKey.currentState?.validate() == true) {
             formKey.currentState?.save();
             Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              return RandomizerPage(min: _min, max: _max);
+              return RandomizerPage(min: min.value, max: max.value);
             }));
           }
         },
