743aa8ae12d9749a640c355d6a1001007c2e9616
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-05-28 11:40:53 -0600

    go_router configurado

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -13,14 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 3).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons': (context) => const BottonsScreen(),
-        '/cards': (context) => const CardsScreen(),
-      },
     );
   }
 }
--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -48,7 +49,7 @@ class _CustomListTile extends StatelessWidget {
       ),
       trailing: const Icon(Icons.arrow_forward_ios_rounded),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
