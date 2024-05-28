c9d363823797db15b2b98beae5960417a4f00fe5
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-05-28 11:27:35 -0600

    Rutas basicas configuradas.

---
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottonsScreen extends StatelessWidget {
+  const BottonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons Screem'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -47,7 +48,7 @@ class _CustomListTile extends StatelessWidget {
       ),
       trailing: const Icon(Icons.arrow_forward_ios_rounded),
       onTap: () {
-        //TODO: Navegar a otra pantalla
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
