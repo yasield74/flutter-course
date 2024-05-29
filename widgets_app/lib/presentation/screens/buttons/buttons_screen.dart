692f5be2d7964e61cb610f1c41462f06abb44820
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-05-28 23:22:52 -0600

    Custom button added

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -73,7 +73,33 @@ class _ButtonsView extends StatelessWidget {
                   icon: const Icon(Icons.app_registration_rounded),
                   style: ButtonStyle(
                       backgroundColor: WidgetStatePropertyAll(colors.primary))),
+              const CustomButton(),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () => {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Custom Button',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
