6983ccbdd88863d2a16a97e60ba4e92121ee7036
Yasiel Duenas Sanchez <yasield74@gmail.com>
2024-08-24 09:25:38 -0600

    addmin Next button and some animations.

--- widgets_app/lib/presentation/screens/tutorial/tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/tutorial/tutorial_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -31,10 +32,38 @@ class SlideInfo {
   });
 }
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   static const name = '/tutorial';
 
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  late PageController pageviewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +72,7 @@ class AppTutorialScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
+            controller: pageviewController,
             physics: const BouncingScrollPhysics(),
             children: slides
                 .map((slideData) => _Slide(
@@ -59,7 +89,17 @@ class AppTutorialScreen extends StatelessWidget {
               child: const Text('Exit'),
               onPressed: () => context.pop(),
             ),
-          )
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                      from: 15,
+                      delay: const Duration(seconds: 1),
+                      child: FilledButton(
+                          onPressed: () {}, child: const Text('Next'))))
+              : const SizedBox()
         ],
       ),
     );
